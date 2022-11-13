a964248a5665d392d505051c564c1d8e529c1b13
Hernán Federico Costarelli <hernancostarelli@gmail.com>
2022-11-13 18:34:26 -0300

    feature (dependencies): dependencies required for project start-up CHANGES IN: pom.xml

---
+++ src/test/java/com/noCountry/C815MJavaReact/PhotoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noCountry.C815MJavaReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
