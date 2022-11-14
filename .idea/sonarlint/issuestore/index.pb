8a9248c9ef06d0d873effa1d148df48f7cf8af54
Hernán Federico Costarelli <hernancostarelli@gmail.com>
2022-11-14 12:49:47 -0300

    feature (addition) : Add package structure and validations (Regex)

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,5 +3,3 @@
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 s
 Csrc/main/java/com/noCountry/C815MJavaReact/PhotoWebApplication.java,8\d\8dd5215fa5eb5980f9aed5ec321be1c91bb432d8
-7
-pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/com/noCountry/C815MJavaReact/model/user.java
+++ src/main/java/com/noCountry/C815MJavaReact/model/user.java
@@ -1,4 +1,42 @@
 package com.noCountry.C815MJavaReact.model;
 
-public class user {
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Table(name = "user")
+@EqualsAndHashCode
+public class user implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(unique = true)
+    protected String id;
+
+    @Column(name = "name")
+    protected String name;
+
+    @Column(name = "surname")
+    protected String surname;
+
+    @Column(name = "document", unique = true)
+    protected String document;
+
+    @Column(name = "email", unique = true)
+    protected String email;
+
+    @Column(name = "soft_delete")
+    protected boolean softDelete = Boolean.FALSE;
 }
