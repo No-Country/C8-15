9306c9107ba06d35c4797cda4b63ce8ef558942d
Gaston Garcia <alejandro.zigarancontacto@gmail.com>
2022-11-23 12:13:08 -0300

    added handleErrors

---
+++ src/main/java/com/nocountry/java_react/exception/ControllerExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.nocountry.java_react.exception;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+        @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+
+            
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
+    /*Ejemplo value={} cclase de la excepcion a manejar
+
+    
+    @ExceptionHandler(value = {AlreadyExistsException.class})
+    protected ResponseEntity<Object> handleAlreadyExistException(AlreadyExistsException ex,
+                                                                 WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST); dependiendo del error va a cambiar el httpStatus badrequest,forbidden,not found,ect
+    }
+    
+    */
+}
