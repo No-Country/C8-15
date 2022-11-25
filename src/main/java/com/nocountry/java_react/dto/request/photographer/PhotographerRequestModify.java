8237eacbe66d497d769a33e6b802bd37788bc4e1
Hernan Federico Costarelli <hernancostarelli@gmail.com>
2022-11-25 13:01:03 -0300

    feat(Photographer): Validations in the photographer's request
    Signed-off-by: Hernan Federico Costarelli <hernancostarelli@gmail.com>

--- src/main/java/com/nocountry/java_react/dto/request/photographer/PhotographerRequestModify.java
+++ src/main/java/com/nocountry/java_react/dto/request/photographer/PhotographerRequestModify.java
@@ -1,16 +1,46 @@
 package com.nocountry.java_react.dto.request.photographer;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.nocountry.java_react.validation.StartingWithBlankSpaces;
 import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class PhotographerRequestModify {
-    
+
+    @NotBlank(message = "EL NOMBRE DEL FOTÓGRAFO NO PUEDE ESTAR VACÍO O SER NULO")
+    @Size(min = 3, max = 50, message = "EL NOMBRE DEL FOTÓGRAFO DEBE TENER UN MÍNIMO DE 3 LETRAS Y UN MÁXIMO DE 50")
+    @StartingWithBlankSpaces(message = "EL NOMBRE DEL FOTÓGRAFO NO DEBE COMENZAR CON ESPACIOS EN BLANCO")
+    @JsonProperty("name")
     private String name;
+
+    @NotBlank(message = "EL NOMBRE DEL FOTÓGRAFO NO PUEDE ESTAR VACÍO O SER NULO")
+    @Size(min = 3, max = 50, message = "EL NOMBRE DEL FOTÓGRAFO DEBE TENER UN MÍNIMO DE 3 LETRAS Y UN MÁXIMO DE 50")
+    @StartingWithBlankSpaces(message = "EL NOMBRE DEL FOTÓGRAFO NO DEBE COMENZAR CON ESPACIOS EN BLANCO")
+    @JsonProperty("surname")
     private String surname;
+
+    @NotBlank(message = "EL CORREO ELECTRÓNICO DEL FOTÓGRAFO NO PUEDE ESTAR VACÍO O SER NULO")
+    @Email(message = "EL CORREO ELECTRÓNICO DEL FOTÓGRAFO DEBE TENER UN FORMATO CORRECTO")
+    @StartingWithBlankSpaces(message = "EL CORREO ELECTRÓNICO DEL FOTÓGRAFO NO DEBE COMENZAR CON ESPACIOS EN BLANCO")
+    @JsonProperty("email")
     private String email;
+
+    @JsonProperty("telephone")
     private String telephone;
+
+    @JsonProperty("city")
     private String city;
+
+    @JsonProperty("country")
     private String country;
+
+    @JsonProperty("facebookUrl")
     private String facebookUrl;
+
+    @JsonProperty("instagramUrl")
     private String instagramUrl;
 }
--- src/main/java/com/nocountry/java_react/dto/request/photographer/PhotographerRequestPassword.java
+++ src/main/java/com/nocountry/java_react/dto/request/photographer/PhotographerRequestPassword.java
@@ -1,11 +1,30 @@
 package com.nocountry.java_react.dto.request.photographer;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.nocountry.java_react.validation.Password;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class PhotographerRequestPassword {
 
+    @NotBlank(message = "EL PASSWORD ANTERIOR DEL FOTÓGRAFO NO PUEDE ESTAR VACÍO O SER NULO")
+    @Size(min = 6, max = 16, message = "EL PASSWORD ANTERIOR DEL FOTÓGRAFO DEBE TENER UN MÍNIMO DE 6 CARACTERES Y UN MÁXIMO DE 16")
+    @Password(message = "EL PASSWORD ANTERIOR DEL FOTÓGRAFO DEBE TENER AL MENOS 1 CARÁCTER EN MAYÚSCULA, 1 CARÁCTER EN MINÚSCULA, 1 NÚMERO Y 1 CARÁCTER ESPECIAL (EJEMPLO: 1Password$)")
+    @JsonProperty("oldPassword")
     private String oldPassword;
+
+    @NotBlank(message = "EL PASSWORD DEL FOTÓGRAFO NO PUEDE ESTAR VACÍO O SER NULO")
+    @Size(min = 6, max = 16, message = "EL PASSWORD DEL FOTÓGRAFO DEBE TENER UN MÍNIMO DE 6 CARACTERES Y UN MÁXIMO DE 16")
+    @Password(message = "EL PASSWORD DEL FOTÓGRAFO DEBE TENER AL MENOS 1 CARÁCTER EN MAYÚSCULA, 1 CARÁCTER EN MINÚSCULA, 1 NÚMERO Y 1 CARÁCTER ESPECIAL (EJEMPLO: 1Password$)")
+    @JsonProperty("password")
     private String password;
+
+    @NotBlank(message = "LA CONFIRMACIÓN DEL PASSWORD DEL FOTÓGRAFO NO PUEDE ESTAR VACÍO O SER NULO")
+    @Size(min = 6, max = 16, message = "LA CONFIRMACIÓN DEL PASSWORD DEL FOTÓGRAFO DEBE TENER UN MÍNIMO DE 6 CARACTERES Y UN MÁXIMO DE 16")
+    @Password(message = "LA CONFIRMACIÓN DEL PASSWORD DEL FOTÓGRAFO DEBE TENER AL MENOS 1 CARÁCTER EN MAYÚSCULA, 1 CARÁCTER EN MINÚSCULA, 1 NÚMERO Y 1 CARÁCTER ESPECIAL (EJEMPLO: 1Password$)")
+    @JsonProperty("confirmPassword")
     private String confirmPassword;
 }
